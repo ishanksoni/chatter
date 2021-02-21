d1d67fe132a719d619a93a45a441d36a4c03be0f
ishanksoni <ishanksoni8696@gmail.com>
2021-02-22 01:58:26 +0530

    chatroom app fixed

--- core/settings.py
+++ core/settings.py
@@ -120,3 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+ASGI_APPLICATION = "core.routing.application"
+
+CHANNEL_LAYERS  = {
+    "default" : {   
+        "BACKEND" : "channels.layers.InMemoryChannelLayer"
+    }
+}
